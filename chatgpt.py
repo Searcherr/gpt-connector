de9c17948e33ecb28f811ad3de9a78bc8fd8ff25
Searcherr <alfafemida@gmail.com>
2023-06-30 21:50:57 +0200

    Just started

--- chatgpt.py
+++ chatgpt.py
@@ -1,11 +1,38 @@
-from apikey import apikey
 import os
-import sys
+import streamlit as st
+import streamlit as streamlit
 
+from apikey import apikey
 from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
 
-os.environ["OPEN_AI_KEY"] = apikey
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables=["topic"],
+    template="Who is the president of {topic}"
+)
 
-query = sys.argv[1]
-print(query)
+script_template = PromptTemplate(
+    input_variables=["title"],
+    template="Write me a short biography of the president of the following country: {title}"
+)
+
+os.environ["OPENAI_API_KEY"] = apikey
+
+#App framework
+st.title("🦜️🔗 LangChain Testing")
+prompt = st.text_input("The place for your prompt is down below:")
+
+# LLMs
+llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key="title")
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key="script")
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=["topic"],
+                                   output_variables=["title", "script"], verbose=True)
+
+# Showing the answer to the prompt
+if prompt:
+    response = sequential_chain.run({"topic": prompt}, return_only_outputs=True)
+    st.write(response["title"])
+    st.write(response["script"])
--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import streamlit as streamlit
 
 from apikey import apikey
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SimpleSequentialChain
+
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables=["topic"],
+    template="Who is the president of {topic}"
+)
+
+script_template = PromptTemplate(
+    input_variables=["title"],
+    template="Write me a short biography of the president of the following country: {title}"
+)
 
 os.environ["OPENAI_API_KEY"] = apikey
 
@@ -13,8 +26,11 @@ prompt = st.text_input("The place for your prompt is down below:")
 
 # LLMs
 llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
+sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True)
 
 # Showing the answer to the prompt
 if prompt:
-    response = llm(prompt)
+    response = sequential_chain.run(prompt)
     st.write(response)
